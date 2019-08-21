4da159b6f078927dbaebc6d4af14cbdf46de1c90
Jordan <jordandoan@hotmail.com>
2019-08-21 15:43:37 -0700

    Styled member card and navbar

--- src/components/Form/Members.js
+++ src/components/Form/Members.js
@@ -1,20 +1,38 @@
 import React from "react";
+import styled from "styled-components";
 
 const Members = (props) => {
     let handleClick = (member, index) => {
         props.setEdit({...member, ["index"]: index});
         props.history.push("/form");
     }
+    let Container = styled.div`
+        display:flex;
+        width:1000px;
+        justify-content:center;
+        margin:auto;
+        flex-wrap:wrap;
+    `
+    let Card = styled.div`
+        background-color:lightblue;
+        width:300px;
+        height:200px;
+        border-radius:10px;
+        margin:5px;
+    `
     return (
-        <div>I am the members list.
+        <div>
+            <h2>Members</h2>
+            <Container>
             {props.members.map((member,index) => 
-                <div key={index}>
+                <Card key={index}>
                     <p>Name: {member.name}</p>
                     <p>Email: {member.email}</p>
                     <p>Role: {member.role}</p>
                     <button onClick={() => handleClick(member,index)}>Edit</button>
-                </div>
+                </Card>
             )}
+            </Container>
         </div>
     )
 }
--- src/components/Nav/NavBar.js
+++ src/components/Nav/NavBar.js
@@ -1,15 +1,21 @@
 import React from "react";
 import {NavLink} from "react-router-dom";
+import styled from "styled-components";
 
 const NavBar = () => {
+    let Nav = styled.nav`
+        display:flex;
+        align-items:center;
+        justify-content:center;
+    `
     return (
-        <nav>
+        <Nav>
             <h1>TEAM BUILDER</h1>
-            <div>
+            <div className="links">
                 <NavLink to="/">Home</NavLink>
                 <NavLink to="/form">Add Member</NavLink>
             </div>
-        </nav>
+        </Nav>
     );
 }
 
